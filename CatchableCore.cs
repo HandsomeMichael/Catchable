c1351338bc1e8aa5548cafa0f2a81d39061bd99c
Handsome Michael <70310191+HandsomeMichael@users.noreply.github.com>
2024-12-12 22:10:50 +0800

    coding

--- CatchableCore.cs
+++ CatchableCore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
@@ -12,11 +13,47 @@ namespace Catchable
 {
 	public struct CatchTypeNPC
 	{
+		public const int InvalidID = -696969;
 		internal string _mod;
 		internal string _name;
 		internal int _id;
 
-		public bool IsInvalid() => GetLiteralID() == -6969;
+		public CatchTypeNPC(NPC npc)
+		{
+			if (npc == null)
+			{
+				throw new ArgumentNullException("NPC is Null when creating new CatchTypeNPC");
+			}
+
+			_id = npc.type;
+
+			if (npc.ModNPC != null)
+			{
+				_mod = npc.ModNPC.Mod.Name;
+				_name = npc.ModNPC.Name;
+			}
+			else
+			{
+				_mod = "Terraria";
+				_name = "";
+			}
+		}
+
+		public void SaveData(TagCompound tag)
+		{
+			tag.Add("mod",_mod);
+			tag.Add("name",_name);
+			tag.Add("id",_id);
+		}
+
+		public void LoadData(TagCompound tag)
+		{
+			_mod = tag.GetString("mod");
+			_name = tag.GetString("name");
+			_id = tag.GetInt("id");
+		}	
+
+		public bool IsInvalid() => GetLiteralID() == InvalidID;
 
 		public int GetLiteralID()
 		{
@@ -33,24 +70,33 @@ namespace Catchable
 					Main.NewText("Im sorry but i think you removed some mods, yeah thats it lol");
 					_id = npc.Type;
 				}
+
+				// Might remove if my friend give me nachos
+				if (npc.Type > NPCLoader.NPCCount)
+				{
+					throw new Exception("Uhhh. did you broke tmodloader");
+				}
+
 				return npc.Type;
 			}
 
-			return -6969;
+			// I remember terraria somehow had negative ids so just incase
+			return InvalidID;
 		}
 	}
+
 	public class CatchedItem : ModItem
 	{
 		public CatchTypeNPC catchType;
 
         public override void SaveData(TagCompound tag)
         {
-            base.SaveData(tag);
+            catchType.SaveData(tag);
         }
 
         public override void LoadData(TagCompound tag)
         {
-            base.LoadData(tag);
+            catchType.LoadData(tag);
         }
 
         public override bool CanStack(Item source)
@@ -65,9 +111,9 @@ namespace Catchable
             return false;
         }
 
-        public override void SetDefaults()
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
         {
-            base.SetDefaults();
+            return false;
         }
     }
 }
